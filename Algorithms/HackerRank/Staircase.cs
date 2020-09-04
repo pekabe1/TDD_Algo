6c6f9a65e102a46d2c0864651fc2f3c5695fc0f8
KacperZiarkowski <PLUJKV3@amway.com>
2020-09-04 17:20:40 +0200

    Ready solution

---
+++ Algorithms/HackerRank/Staircase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.HackerRank
+{
+    public class Staircase
+    {
+        public void BuildStaircase(int n)
+        {
+           
+
+            //string stairCaseFloor = staircaseBuilder[0];
+            //for (int i = 0; i < n; i++)
+            //{
+            //    staircaseBuilder.Add(stairCaseFloor.Substring(i + 1, n - 1));
+            //}
+
+            //foreach (var staircaseLevel in staircaseBuilder)
+            //{
+            //    Console.WriteLine(staircaseLevel);
+            //}
+        }
+        public string CreateStaircaseFloor(int n)
+        {
+            List<string> staircaseBuilder = new List<string> { "" };
+
+            for (int i = 0; i < n; i++)
+            {
+                staircaseBuilder[0] += "#";
+            }
+            return staircaseBuilder[0];
+
+        }
+
+
+    }
+}
---
+++ TDD_Algo/HackerRankTests/StaircaseTests.cs
@@ -0,0 +1,28 @@
+﻿using Algorithms.HackerRank;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDD_Algo.HackerRankTests
+{
+    [TestFixture]
+    class StaircaseTests
+    {
+
+        [Test]
+        [TestCase(6)]
+        [TestCase(12)]
+        [TestCase(190)]
+        public void GenerateStaircaseBaseFloor_Returns_BaseFloorWithLengthEqualN(int n)
+        {
+            var staircase = new Staircase();
+            
+            var baseFloor = staircase.CreateStaircaseFloor(n);
+
+            Assert.AreEqual(n, baseFloor.Length);
+
+        }
+        
+    }
+}
